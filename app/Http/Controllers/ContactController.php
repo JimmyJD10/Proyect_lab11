2134926cc356a0e479c225dd2a1362211adca014
JimmyJD10 <168223271+JimmyJD10@users.noreply.github.com>
2024-05-31 09:39:41 -0500

    Comit actualizado

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,38 +10,6 @@ use Illuminate\Support\Facades\Storage;
 
 class ContactController extends Controller
 {
-    public function edit(Contact $contact): View
-    {
-        return view('contacts.edit', [
-            'contact' => $contact,
-        ]);
-    }
-
-    public function update(Request $request, Contact $contact): RedirectResponse
-    {
-        $validated = $request->validate([
-            'first_name' => 'required|regex:/^[\pL\s]+$/u|max:30',
-            'last_name' => 'nullable|regex:/^[\pL\s]+$/u|max:30',
-            'phone' => 'required|digits:9',
-            'email' => 'nullable|email:strict',
-            'address' => 'nullable|string|max:255',
-            'company' => 'nullable|string|max:255',
-            'notes' => 'nullable|string',
-            'photo' => 'nullable|image|max:2048'
-        ]);
-
-        if ($request->hasFile('photo')) {
-            if ($contact->photo_path) {
-                Storage::delete($contact->photo_path);
-            }
-            $validated['photo_path'] = $request->file('photo')->store('photos', 'public');
-        }
-
-        $contact->update($validated);
-
-        return redirect(route('contacts.index'));
-    }
-
     public function index(Request $request): View
     {
         return view('contacts.index', [
@@ -81,5 +49,48 @@ class ContactController extends Controller
         return view('contacts.show', [
             'contact' => $contact,
         ]);
+    }
+
+    public function edit(Contact $contact): View
+    {
+        return view('contacts.edit', [
+            'contact' => $contact,
+        ]);
+    }
+
+    public function update(Request $request, Contact $contact): RedirectResponse
+    {
+        $validated = $request->validate([
+            'first_name' => 'required|regex:/^[\pL\s]+$/u|max:30',
+            'last_name' => 'nullable|regex:/^[\pL\s]+$/u|max:30',
+            'phone' => 'required|digits:9',
+            'email' => 'nullable|email:strict',
+            'address' => 'nullable|string|max:255',
+            'company' => 'nullable|string|max:255',
+            'notes' => 'nullable|string',
+            'photo' => 'nullable|image|max:2048'
+        ]);
+
+        if ($request->hasFile('photo')) {
+            if ($contact->photo_path) {
+                Storage::delete($contact->photo_path);
+            }
+            $validated['photo_path'] = $request->file('photo')->store('photos', 'public');
+        }
+
+        $contact->update($validated);
+
+        return redirect(route('contacts.index'));
+    }
+
+    public function destroy(Contact $contact): RedirectResponse
+    {
+        if ($contact->photo_path) {
+            Storage::delete($contact->photo_path);
+        }
+
+        $contact->delete();
+
+        return redirect(route('contacts.index'))->with('success', 'Contacto eliminado exitosamente.');
     }
 }
